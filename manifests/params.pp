3d20341deb06e85f95c896b9711a8a9c3fdef669
Lars Tobias Skjong-Børsting <larstobi@conduct.no>
2013-05-16 15:02:38 +0200

    The 'directory' parameter has different path on debian systems.
    The directory /var/named is not created by the bind9 package on debian systems.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,28 +6,30 @@ class bind::params {
     'RedHat',
     'CentOS',
     'Amazon': {
-      $packagenameprefix = 'bind'
-      $servicename       = 'named'
-      $binduser          = 'root'
-      $bindgroup         = 'named'
-      $rfc1912_file      = '/etc/named.rfc1912.zones'
+      $packagenameprefix  = 'bind'
+      $servicename        = 'named'
+      $binduser           = 'root'
+      $bindgroup          = 'named'
+      $rfc1912_file       = '/etc/named.rfc1912.zones'
+      $directory          = '/var/named'
     }
     'Debian',
     'Ubuntu': {
-      $packagenameprefix = 'bind9'
-      $servicename       = 'bind9'
-      $binduser          = 'bind'
-      $bindgroup         = 'bind'
-      $rfc1912_file      = '/etc/bind/zones.rfc1918'
+      $packagenameprefix  = 'bind9'
+      $servicename        = 'bind9'
+      $binduser           = 'bind'
+      $bindgroup          = 'bind'
+      $rfc1912_file       = '/etc/bind/zones.rfc1918'
+      $directory          = '/var/cache/bind'
     }
     default: {
-      $packagenameprefix = 'bind'
-      $servicename       = 'named'
-      $binduser          = 'root'
-      $bindgroup         = 'named'
-      $rfc1912_file      = '/etc/named.rfc1912.zones'
+      $packagenameprefix  = 'bind'
+      $servicename        = 'named'
+      $binduser           = 'root'
+      $bindgroup          = 'named'
+      $rfc1912_file       = '/etc/named.rfc1912.zones'
+      $directory          = '/var/named'
     }
   }
 
 }
-
